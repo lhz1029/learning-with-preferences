f30a51cb81c769aeb223258a1a3268c817b17100
lhz1029 <lilyhzhang1029@gmail.com>
2024-03-03 17:29:04 -0500

    add callback to log predictions when packing=False

--- examples/research_projects/preference_learning/train.py
+++ examples/research_projects/preference_learning/train.py
@@ -17,6 +17,7 @@ from transformers import (
     TrainingArguments,
     set_seed,
 )
+from transformers.integrations import WandbCallback
 
 from trl import SFTTrainer
 from trl.import_utils import is_npu_available, is_xpu_available
@@ -182,6 +183,57 @@ def create_datasets(tokenizer, args, seed=None):
         valid_datasets[k] = eval_datasets[k]
     return train_dataset, valid_datasets
 
+def decode_predictions(tokenizer, predictions):
+    labels = tokenizer.batch_decode(predictions.label_ids)
+    prediction_text = tokenizer.batch_decode(predictions.predictions.argmax(axis=-1))
+    return {"labels": labels, "predictions": prediction_text}
+
+
+class WandbPredictionProgressCallback(WandbCallback):
+    """Custom WandbCallback to log model predictions during training.
+
+    This callback logs model predictions and labels to a wandb.Table at each logging step during training.
+    It allows to visualize the model predictions as the training progresses.
+
+    Attributes:
+        trainer (Trainer): The Hugging Face Trainer instance.
+        tokenizer (AutoTokenizer): The tokenizer associated with the model.
+        sample_dataset (Dataset): A subset of the validation dataset for generating predictions.
+        num_samples (int, optional): Number of samples to select from the validation dataset for generating predictions. Defaults to 100.
+    """
+
+    def __init__(self, trainer, tokenizer, val_dataset, num_samples=100, freq=2):
+        """Initializes the WandbPredictionProgressCallback instance.
+
+        Args:
+            trainer (Trainer): The Hugging Face Trainer instance.
+            tokenizer (AutoTokenizer): The tokenizer associated with the model.
+            val_dataset (Dataset): The validation dataset.
+            num_samples (int, optional): Number of samples to select from the validation dataset for generating predictions. Defaults to 100.
+            freq (int, optional): Control the frequency of logging. Defaults to 2.
+        """
+        super().__init__()
+        self.trainer = trainer
+        self.tokenizer = tokenizer
+        self.sample_dataset = val_dataset.select(range(num_samples))
+        self.freq = freq
+
+
+    def on_evaluate(self, args, state, control,  **kwargs):
+        super().on_evaluate(args, state, control, **kwargs)
+        # control the frequency of logging by logging the predictions every `freq` epochs
+        if state.epoch % self.freq == 0:
+          # generate predictions
+          predictions = self.trainer.predict(self.sample_dataset)
+          # decode predictions and labels
+          predictions = decode_predictions(self.tokenizer, predictions)
+          # add predictions to a wandb.Table
+          predictions_df = pd.DataFrame(predictions)
+          predictions_df["epoch"] = state.epoch
+          records_table = self._wandb.Table(dataframe=predictions_df)
+          # log the table to wandb
+          self._wandb.log({"sample_predictions": records_table})
+
 
 if __name__ == "__main__":
     parser = HfArgumentParser((ScriptArguments, TrainingArguments))
@@ -233,7 +285,7 @@ if __name__ == "__main__":
         data_collator = None
     else:
         data_collator = DataCollatorForCompletionOnlyLM(
-            response_template="###Response: ",
+            response_template=[835, 5103, 29901], # " ###Response: ",
             instruction_template="###Instruction: ",
             tokenizer=tokenizer,
             mlm=False,
@@ -251,6 +303,10 @@ if __name__ == "__main__":
         data_collator=data_collator,
         formatting_func=None if script_args.packing else formatting_func,
     )
+    if not script_args.packing:
+        # TODO: enable logging with packing=True by resolving 'ConstantLengthDataset' object has no attribute 'select'
+        progress_callback = WandbPredictionProgressCallback(trainer, tokenizer, eval_datasets["valid"], 3)# 15)
+        trainer.add_callback(progress_callback)
     trainer.train()
     trainer.save_model(training_args.output_dir)
 
